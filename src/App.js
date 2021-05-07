96d7e8cbe51f20010b0caefd2555c32b029d918f
Adheesh Gokhale <adheesh7@Adheeshs-MacBook-Pro.local>
2021-05-07 00:35:09 -0700

    trial

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          App launched
+          App launched 2
         </p>
         <a
           className="App-link"
